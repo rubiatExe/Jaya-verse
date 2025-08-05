d965633dd772900fdb23d3bd128a75f6a64d5281
Rubiat Faisal <rubiat.bin@gmail.com>
2025-08-05 21:26:52 +0000

    it says something went wrong

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export { db, app };
